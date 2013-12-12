9220fdaaaa7415e901dd3d7dc9bdeaf7a8772d50
Chris Eykamp <chris@eykamp.com>
2013-12-12 22:20:22 +0100

    Initial transfer of files to repo

---
+++ PackUp.bat
@@ -0,0 +1,2 @@
+rem copy "C:\Thunderbird Developer Profile\extensions\{C8534C26-F59A-11DA-9804-B622A1EF5492}\chrome\content\*.xul" "C:\Documents and Settings\Chris\My Documents\My Extensions\folderAccount\folderAccount\chrome\content\"
+"c:\Program Files\7-Zip\7z" a -tzip  ..\folderAccountBeta.xpi *  -x!PackUp.bat
